f26046ef9d89904a22379ef3e403dad6cab57351
amAuditoria <136050576+amAuditoria@users.noreply.github.com>
2023-06-10 20:40:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-document.getElementById("1").onclick = () => {changeDisplay("about")};
-document.getElementById("2").onclick = () => {changeDisplay("purpose")};
-document.getElementById("3").onclick = () => {changeDisplay("benefits")};
-document.getElementById("4").onclick = () => {changeDisplay("retro")};
-document.getElementById("5").onclick = () => {changeDisplay("contact")};
+document.getElementById("tab1").onclick = () => {changeDisplay("about")};
+document.getElementById("tab11").onclick = () => {changeDisplay("about")};
+document.getElementById("tab2").onclick = () => {changeDisplay("purpose")};
+document.getElementById("tab22").onclick = () => {changeDisplay("purpose")};
+document.getElementById("tab3").onclick = () => {changeDisplay("benefits")};
+document.getElementById("tab33").onclick = () => {changeDisplay("benefits")};
+document.getElementById("tab4").onclick = () => {changeDisplay("retro")};
+document.getElementById("tab44").onclick = () => {changeDisplay("retro")};
+document.getElementById("tab5").onclick = () => {changeDisplay("recurse")};
+document.getElementById("tab55").onclick = () => {changeDisplay("recurse")};
+document.getElementById("tab6").onclick = () => {changeDisplay("contact")};
+document.getElementById("tab66").onclick = () => {changeDisplay("contact")};
+
+window.onload = changeDisplay("about")
 
 function changeDisplay(id) {
     for (let element of document.getElementsByClassName("content"))
